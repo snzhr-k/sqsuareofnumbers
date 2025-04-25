8a3155ebc5b10e7ee22056742fbd01a614591a9d
snzhrk <sanzhar.kazakstan@gmail.com>
2025-04-25 13:44:34 +0200

    final changes

--- project_code.py
+++ project_code.py
@@ -15,56 +15,46 @@
 # # Do not use any for loop, while loop, or any list / set / dictionary comprehension
 # # Your solution will be tested against Python 3.13 (as of January 2025) or higher
 
-#Implementation of the feature that reads inputs
-def ReadN(N, arr): #reads # of test cases
+def ReadN(N, arr): 
     if N == 0:
         return arr
     elif N > 0: 
         ReadX(int(input()), arr)
         ReadN(N-1, arr) 
 
-def ReadX(X, intarr): #reads # of integers and line of integers
+def ReadX(X, intarr):
     return intarr.append(input())
 
-#Implementation of the feature that calculates the sum of squares of a given list of integers
 def SumOfSquares(arr):
     squaredInts = list(map(SquareNumber, arr))
     sumInts = list(map(sum, squaredInts))
     return sumInts
 
-def SquareNumber(Ints): #Ints is a list of integer elements
-    #caluclates sum of squares for one line of integers
-    filteredInts = list(filter(lambda x: x>0, Ints)) #filtering neagative numbers
+def SquareNumber(Ints): 
+    filteredInts = list(filter(lambda x: x>0, Ints)) 
     squaredInts = list(map(lambda x: x*x, filteredInts))
     squaredInts = list(filter(lambda x: x>0, squaredInts))
 
     return squaredInts
     
 
-#Implementation of the feature that parses string lists into integer lists
 def decodeString(arr):
-    # Split each string in arr: ["1 2", "3 4"] → [["1", "2"], ["3", "4"]]
     split_strs = list(map(lambda x: x.split(' '), arr))
-    
-    # Convert each inner string to an int → [[1, 2], [3, 4]]
     int_lists = list(map(lambda lst: list(map(int, lst)), split_strs))
     
     return int_lists
 
-#Initializing and testing functions
 def main():
     test_case_lines = []
     N = int(input())
-
     ReadN(N, test_case_lines)
 
     decoded = decodeString(test_case_lines)
-
     sum_of_squares = SumOfSquares(decoded)
     
     print(*sum_of_squares, sep="\n")
+
 if __name__ == "__main__":
     main()
-
 
 
